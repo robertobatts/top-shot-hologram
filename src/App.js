134d50d5f3a4b33581c1dc3c9e5f34e17dcf9395
Roberto Battaglia <battaroberto@gmail.com>
2021-03-20 11:57:00 +0100

    init cube with babylon.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Input, Button } from '@material-ui/core';
+import Cube from './Cube'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div >
+          <Input placeholder="https://nbatopshot.com/moment/hustlewestbrook+73c2eb70-8ff3-4719-99e9-f57edc4196df" onChange={(event) => this.handleLinkInputChange(event.target.value)} />
+          <Button disabled={false} onClick={() => this.triggerProcess()} variant="contained" color="primary">Trigger</Button>
+        </div>
+        <div className="cube-container">
+          <Cube></Cube>
+        </div>
+      </div>
+    );
+  }
+
+  handleLinkInputChange(newLink) {
+    if (newLink !== this.state.link) {
+      this.setState({ link: newLink });
+    }
+  }
+
+  triggerProcess() {
+    console.log(this.state.link)
+  }
 }
 
-export default App;
