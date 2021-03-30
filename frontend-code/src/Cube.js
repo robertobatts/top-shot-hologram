df1785b30ebbc950b63b7e4fe783153f30679e37
Roberto Battaglia <battaroberto@gmail.com>
2021-03-30 21:10:21 +0200

    cube glowing borders, with color retrieved from db

--- frontend-code/src/Cube.js
+++ frontend-code/src/Cube.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Engine, Scene } from 'react-babylonjs';
-import { ArcRotateCamera, MeshBuilder, HemisphericLight, Vector3, Mesh, StandardMaterial, Color3, VideoTexture, Texture, BoundingInfo } from '@babylonjs/core';
+import {
+  ArcRotateCamera, MeshBuilder, HemisphericLight, Vector3, Mesh, StandardMaterial, Color3, Color4,
+  VideoTexture, Texture, BoundingInfo, Path3D, GlowLayer, FresnelParameters
+} from '@babylonjs/core';
 import webHadlers from "./utils/webHandlers";
 
 var copyScene;
@@ -33,14 +36,27 @@ export default class Cube extends React.Component {
       var photo5 = copyScene.getMeshByName("photo5");
       photo5.material.diffuseTexture = new Texture(webHadlers.getTopShotVideoLink(this.props.selectedCube.mediaIdsAsString[5]), copyScene);
       photo5.material.alpha = 1;
-      if (!this.props.selectedCube.borderColor) {
-        copyScene.getBoundingBoxRenderer().enabled = false;
-      } else {
-        copyScene.getBoundingBoxRenderer().enabled = true;
-        copyScene.getBoundingBoxRenderer().frontColor = Color3.FromInts(this.props.selectedCube.borderColor.r, this.props.selectedCube.borderColor.g, this.props.selectedCube.borderColor.b);
 
+      var glow = copyScene.getGlowLayerByName("glow");
+      var border1 = copyScene.getMeshByName("border1");
+      var border2 = copyScene.getMeshByName("border2");
+      if (!this.props.selectedCube.borderColor) {
+        glow.intensity = 0;
+        border1.isVisible = false;
+        border2.isVisible = false;
+      } else {
+        border1.isVisible = true;
+        border2.isVisible = true;
+        glow.intensity = 1.5;
+        var color = Color3.FromInts(this.props.selectedCube.borderColor.r, this.props.selectedCube.borderColor.g, this.props.selectedCube.borderColor.b);
+        glow.customEmissiveColorSelector = function (mesh, subMesh, material, result) {
+          if (mesh.name === "border1" || mesh.name === "border2") {
+            result.set(color.r, color.g, color.b, 1);
+          } else {
+            result.set(0, 0, 0, 0);
+          }
+        }
       }
-      //video.computeWorldMatrix();
     }
   }
 
@@ -68,8 +84,6 @@ export default class Cube extends React.Component {
     video.material = new StandardMaterial("material1", scene);
     video.material.diffuseColor = new Color3(1, 1, 1);
     video.material.alpha = .8;
-    video.setBoundingInfo(new BoundingInfo(new Vector3(-2.7, -2.7, 0), new Vector3(2.7, 2.7, 0)));
-    video.showBoundingBox = true;
 
     var photo1 = MeshBuilder.CreatePlane("photo1", { size: 5, sideOrientation: Mesh.DOUBLESIDE }, scene);
     photo1.position.set(0, 0, 2.7);
@@ -77,8 +91,6 @@ export default class Cube extends React.Component {
     photo1.material = new StandardMaterial("material2", scene);
     photo1.material.diffuseColor = new Color3(1, 1, 1);
     photo1.material.alpha = .8;
-    photo1.setBoundingInfo(new BoundingInfo(new Vector3(2.7, 2.7, 0), new Vector3(-2.7, -2.7, 0)));
-    photo1.showBoundingBox = true;
 
     var photo2 = MeshBuilder.CreatePlane("photo2", { size: 5, sideOrientation: Mesh.DOUBLESIDE }, scene);
     photo2.position.set(2.7, 0, 0);
@@ -86,8 +98,6 @@ export default class Cube extends React.Component {
     photo2.material = new StandardMaterial("material3", scene);
     photo2.material.diffuseColor = new Color3(1, 1, 1);
     photo2.material.alpha = .8;
-    photo2.setBoundingInfo(new BoundingInfo(new Vector3(-2.7, -2.7, 0), new Vector3(2.7, 2.7, 0)));
-    photo2.showBoundingBox = true;
 
     var photo3 = MeshBuilder.CreatePlane("photo3", { size: 5, sideOrientation: Mesh.DOUBLESIDE }, scene);
     photo3.position.set(0, 2.7, 0);
@@ -95,8 +105,6 @@ export default class Cube extends React.Component {
     photo3.material = new StandardMaterial("material4", scene);
     photo3.material.diffuseColor = new Color3(1, 1, 1);
     photo3.material.alpha = .8;
-    photo3.setBoundingInfo(new BoundingInfo(new Vector3(-2.7, -2.7, 0), new Vector3(2.7, 2.7, 0)));
-    photo3.showBoundingBox = true;
 
     var photo4 = MeshBuilder.CreatePlane("photo4", { size: 5, sideOrientation: Mesh.DOUBLESIDE }, scene);
     photo4.position.set(0, -2.7, 0);
@@ -104,8 +112,6 @@ export default class Cube extends React.Component {
     photo4.material = new StandardMaterial("material5", scene);
     photo4.material.diffuseColor = new Color3(1, 1, 1);
     photo4.material.alpha = .8;
-    photo4.setBoundingInfo(new BoundingInfo(new Vector3(2.7, 2.7, 0), new Vector3(-2.7, -2.7, 0)));
-    photo4.showBoundingBox = true;
 
     var photo5 = MeshBuilder.CreatePlane("photo5", { size: 5, sideOrientation: Mesh.DOUBLESIDE }, scene);
     photo5.position.set(-2.7, 0, 0);
@@ -113,11 +119,20 @@ export default class Cube extends React.Component {
     photo5.material = new StandardMaterial("material6", scene);
     photo5.material.diffuseColor = new Color3(1, 1, 1);
     photo5.material.alpha = .8;
-    photo4.setBoundingInfo(new BoundingInfo(new Vector3(2.7, 2.7, 0), new Vector3(-2.7, -2.7, 0)));
-    photo4.showBoundingBox = true;
 
-    scene.getBoundingBoxRenderer().enabled = false;
-    scene.getBoundingBoxRenderer().showBackLines = false;
+    var points1 = [new Vector3(2.7, 2.7, 2.7), new Vector3(2.7, -2.7, 2.7), new Vector3(2.7, -2.7, -2.7),
+    new Vector3(2.7, 2.7, -2.7), new Vector3(2.7, 2.7, 2.7), new Vector3(-2.7, 2.7, 2.7),
+    new Vector3(-2.7, -2.7, 2.7), new Vector3(2.7, -2.7, 2.7)];
+    var points2 = [new Vector3(-2.7, -2.7, -2.7), new Vector3(-2.7, -2.7, 2.7), new Vector3(-2.7, 2.7, 2.7),
+    new Vector3(-2.7, 2.7, -2.7), new Vector3(-2.7, -2.7, -2.7), new Vector3(2.7, -2.7, -2.7),
+    new Vector3(2.7, 2.7, -2.7), new Vector3(-2.7, 2.7, -2.7)];
+    var path3d1 = new Path3D(points1);
+    var border1 = Mesh.CreateLines("border1", path3d1.getPoints(), scene);
+    var path3d2 = new Path3D(points2);
+    var border2 = Mesh.CreateLines("border2", path3d2.getPoints(), scene);
+    var gl = new GlowLayer("glow", scene);
+    gl.addIncludedOnlyMesh(border1);
+    gl.addIncludedOnlyMesh(border2);
 
     scene.getEngine().runRenderLoop(() => {
       if (scene) {
